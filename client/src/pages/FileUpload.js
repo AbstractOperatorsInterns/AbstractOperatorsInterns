0a7dbbdb67e114e336ddbf0445e03f1813857b45
vvijay874 <167625853+vvijay874@users.noreply.github.com>
2025-02-02 10:28:58 -0600

    Vishnu/user dict (#5)
    Created a full user dictionary using a SQLite database
    Created a full sign up / login functionality
    Separated the socialBot and fileUpload pages
    Created a UI for the socialBot and fileUpload page
    Separated the socialBot and fileUpload pages using a BrowserRouter
    Created a unique socialBot object for each user and allows them to access their specific object

---
+++ client/src/pages/FileUpload.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './pages.css';
+
+function FileUpload() {
+    const [currentPage, setCurrentPage] = useState('chat');
+  
+    const handleFileUpload = () => {
+      setCurrentPage('file-upload'); 
+    };
+  
+    const handleGoBack = () => {
+      setCurrentPage('chat');
+    };
+  
+    return (
+      <div className="chat-container">
+          <div>
+            <h1>File Upload Page</h1>
+            <button onClick={handleGoBack}>Social Bot</button>
+          </div>
+      </div>
+      
+    );
+  }
+
+export default FileUpload
